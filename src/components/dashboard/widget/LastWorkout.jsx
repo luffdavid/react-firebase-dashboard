e7519cfbb0c7ede90b2fa7c0844cab60566dc280
DAVID LUFF <luffd@mediamarktsaturn.de>
2024-03-07 12:37:07 +0100

    fix

--- src/components/dashboard/widget/LastWorkout.jsx
+++ src/components/dashboard/widget/LastWorkout.jsx
@@ -1,48 +1,42 @@
-import { Avatar, IconButton, List, ListItem, ListItemAvatar, ListItemText, Typography } from "@mui/material";
+import { Avatar, List, ListItem, ListItemAvatar, ListItemText, Typography } from "@mui/material";
 import MainDrawer from "../../general/drawer/MainDrawer";
 import OpenInNewRoundedIcon from '@mui/icons-material/OpenInNewRounded';
 import { PRIMARY } from "../../general/Constants";
 import { FitnessCenterTwoTone } from "@mui/icons-material";
 
-
 export default function LastWorkout({ workout }) { 
-    const drawerContent = 
-    <List>
-  <ListItem key={workout.id}>
-    <ListItemAvatar>
-      <Avatar sx={{backgroundColor: PRIMARY}}>
-        <FitnessCenterTwoTone  />
-      </Avatar>
-    </ListItemAvatar>
-    <ListItemText
-      primary={
-        <>
-          <Typography variant='h6'>{workout.title}</Typography>
-        </>
-      }
-      secondary={
-        <>
-          <Typography
-            sx={{ display: 'inline' }}
-            component="span"
-            variant="body2"
-          > 
-          {workout.date} <br />
-           from {workout.start} to {workout.end} <br />
-          </Typography>
-        </>
-      }
-    />
-  </ListItem>
-</List>
-        
-    
+    const drawerContent = workout && (
+      <List>
+        <ListItem key={workout.id}>
+          <ListItemAvatar>
+            <Avatar sx={{ backgroundColor: PRIMARY }}>
+              <FitnessCenterTwoTone />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText
+            primary={
+              <Typography variant='h6'>{workout.title}</Typography>
+            }
+            secondary={
+              <Typography
+                sx={{ display: 'inline' }}
+                component="span"
+                variant="body2"
+              > 
+                {workout.date} <br />
+                from {workout.start} to {workout.end} <br />
+              </Typography>
+            }
+          />
+        </ListItem>
+      </List>
+    );
+
   return (
     <div>
-        <div>
+      <div>
         <MainDrawer drawerTitle={"Your last Workout"}  drawerContent={drawerContent} buttonVariant={"text"} drawerOpenButton={<OpenInNewRoundedIcon color="primary" />} />
-        </div>
-       
+      </div>
     </div>
   );
 }
